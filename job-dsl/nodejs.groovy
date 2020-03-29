ee55253ab442ca8f4b9bd810b15f10013b63e61a
devopsmanam <62137404+devopsmanam@users.noreply.github.com>
2020-03-29 15:46:47 +0200

    Update nodejs.groovy

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,8 +1,8 @@
 job('NodeJS example') {
     scm {
-        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('git://github.com/devopsmanam/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+            node / gitConfigEmail('devops.manam@gmail.com')
         }
     }
     triggers {
